4f962db749ed4dca3fa602fcc243c968c9ad9fc5
ananthiPrathi <ananthi2001k@gmail.com>
2025-03-13 13:56:16 -0700

    Primitive data types

---
+++ Datatype.java
@@ -0,0 +1,24 @@
+public class Datatype
+{
+public static void main(String[] args)
+{
+	       byte b= 13;//8bits= 1byte
+		   short s=138;//16bits= 2byte
+           int i = 100;//32bits= 3byte
+		   long l=100000;//64bits= 4byte
+		   double d = 12.34567893456;//32bits= 3byte
+		   float f = 12.34567893456f;//64bits= 4byte
+		   char c = 'c';//16bits= 2byte
+		   boolean bo = true;//8bits= 1byte
+		   
+	
+		      System.out.println(b);
+			  System.out.println(s);
+			  System.out.println(i);
+			  System.out.println(l);
+			  System.out.println(d);
+			  System.out.println(f);
+			  System.out.println(c);
+			  System.out.println(bo);
+}
+}
